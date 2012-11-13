5bb88bc5f7415c5d06b270516ae8cfd15f2bce6c
Svata Novak <324881@mail.muni.cz>
2012-11-13 18:11:28 +0100

    Added a Node class (attributes only for now)

--- src/main/java/cz/muni/fi/publishsubscribe/matchingtree/Node.java
+++ src/main/java/cz/muni/fi/publishsubscribe/matchingtree/Node.java
@@ -1,5 +1,16 @@
 package cz.muni.fi.publishsubscribe.matchingtree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Node {
+
+	private boolean isLeaf;
+
+	private Test<Comparable<?>> test;
+	private Map<String, Node> resultEdges = new HashMap<String, Node>();
+	private Node starNode;
+
+	private Subscription subscription;
 
 }

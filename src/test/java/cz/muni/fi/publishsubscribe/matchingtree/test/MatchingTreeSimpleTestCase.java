3da39a58aae1266156fc6a2e5095e513507a1430
Svata Novak <324881@mail.muni.cz>
2012-11-13 18:54:07 +0100

    Added a test for empty tree

---
+++ src/test/java/cz/muni/fi/publishsubscribe/matchingtree/test/MatchingTreeSimpleTestCase.java
@@ -0,0 +1,29 @@
+package cz.muni.fi.publishsubscribe.matchingtree.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import cz.muni.fi.publishsubscribe.matchingtree.Attribute;
+import cz.muni.fi.publishsubscribe.matchingtree.AttributeValue;
+import cz.muni.fi.publishsubscribe.matchingtree.Event;
+import cz.muni.fi.publishsubscribe.matchingtree.MatchingTree;
+import cz.muni.fi.publishsubscribe.matchingtree.Subscription;
+
+public class MatchingTreeSimpleTestCase {
+
+	@Test
+	public void testEmptyTree() {
+		MatchingTree emptyTree = new MatchingTree();
+		Event event = new Event();
+		event.addAttribute(new Attribute<Long>("processId",
+				new AttributeValue<Long>(1000L, Long.class)));
+		event.addAttribute(new Attribute<String>("applicationName",
+				new AttributeValue<String>("Apache Server", String.class)));
+
+		List<Subscription> subscriptions = emptyTree.match(event);
+		assertTrue(subscriptions.isEmpty());
+	}
+}

37b300c4ba1fde91598e139bdc8df6785ecb32a1
Svata Novak <324881@mail.muni.cz>
2012-11-13 17:38:14 +0100

    Added a Predicate class

--- src/main/java/cz/muni/fi/publishsubscribe/matchingtree/Predicate.java
+++ src/main/java/cz/muni/fi/publishsubscribe/matchingtree/Predicate.java
@@ -1,5 +1,21 @@
 package cz.muni.fi.publishsubscribe.matchingtree;
 
-public class Predicate {
+public class Predicate<T1 extends Comparable<?>> {
+
+	private Test<T1> test;
+	private String result;
+
+	public Predicate(Test<T1> test, String result) {
+		this.test = test;
+		this.result = result;
+	}
+
+	public Test<T1> getTest() {
+		return test;
+	}
+
+	public String getResult() {
+		return result;
+	}
 
 }

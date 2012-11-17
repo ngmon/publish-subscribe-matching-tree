dea1f2a2ba42b67006532fc26e896636c2333fe3
Svata Novak <324881@mail.muni.cz>
2012-11-17 21:08:38 +0100

    Edited Node equals + hashCode

--- src/main/java/cz/muni/fi/publishsubscribe/matchingtree/Node.java
+++ src/main/java/cz/muni/fi/publishsubscribe/matchingtree/Node.java
@@ -62,28 +62,23 @@ public class Node {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		Node node = (Node) o;
+
+		if (id != node.id)
+			return false;
+
+		return true;
 	}
 
 	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Node other = (Node) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+	public int hashCode() {
+		return (int) (id ^ (id >>> 32));
 	}
 
 }

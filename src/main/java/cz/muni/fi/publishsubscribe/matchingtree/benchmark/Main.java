c75dce4b2982bbd3776bd460399c3405d1d27f22
Svata Novak <324881@mail.muni.cz>
2013-01-14 16:08:31 +0100

    Removed some useless and confusing code, fixed benchmark name

--- src/main/java/cz/muni/fi/publishsubscribe/matchingtree/benchmark/Main.java
+++ src/main/java/cz/muni/fi/publishsubscribe/matchingtree/benchmark/Main.java
@@ -5,15 +5,11 @@ public class Main {
 	public static void main(String[] args) {
 		//com.google.caliper.Runner.main(new String[] { "cz.muni.fi.publishsubscribe.countingtree.benchmark.TestBenchmark" });
 		
-		com.google.caliper.Runner.main(new String[] { "cz.muni.fi.publishsubscribe.matchingtree.benchmark.EveryEventLessThanOrEqual" });
+		//com.google.caliper.Runner.main(new String[] { "cz.muni.fi.publishsubscribe.matchingtree.benchmark.EveryEventLessThanOrEqual" });
 		//com.google.caliper.Runner.main(new String[] { "cz.muni.fi.publishsubscribe.matchingtree.benchmark.OneAttributeOperatorsLessThanAndGreaterThanOrEqual" });
-		//com.google.caliper.Runner.main(new String[] { "cz.muni.fi.publishsubscribe.matchingtree.benchmark.TwelveLongAttributesInequality" });
+		com.google.caliper.Runner.main(new String[] { "cz.muni.fi.publishsubscribe.matchingtree.benchmark.TwelveLongAttributesLessThan" });
+		
 		//com.google.caliper.Runner.main(new String[] { "cz.muni.fi.publishsubscribe.matchingtree.benchmark.Subscribe" });
-
-		/*-for (int i = 0; i < 100; i++) {
-			String randomString = RandomStringUtils.random(2, "abcdefghijklmnopqrstuvwxyz");
-			System.out.println(randomString);
-		}*/
 	}
 
 }
